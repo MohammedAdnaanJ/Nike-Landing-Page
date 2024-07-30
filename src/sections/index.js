301bffa5f5bf1f9ae9fb16b70bc6fe606f802531
JAILLA <mohamedadnan081@gmail.com>
2024-07-30 18:13:09 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import Footer from "./Footer";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  PopularProducts,
+  CustomerReviews,
+  Services,
+  Footer,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+};
